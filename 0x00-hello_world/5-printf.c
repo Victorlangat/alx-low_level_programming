0752852f8c22c9749864af0cfda6d3794f8ce1b4
Victorlangat <victorjameskibet09@gmail.com>
2023-09-10 15:37:28 -0700

    hello to printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- prints 'with proper grammar, but the outcome is a piece of art, ' using the printf function
+ * return valeu will be return (0);
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
