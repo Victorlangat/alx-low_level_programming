27a733ee2c7f5ea60b0e0e07691a029f5f0d6cae
Victorlangat <victorjameskibet09@gmail.com>
2023-09-13 02:45:42 -0700

    first if  function code

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main-program will assign random number to variable 'n'
+ * it  checks if number is positive or negative  returns the value and  status
+ */
+int main(void)
+{
+	int n = 0;
+
+	printf("Enter a number\n")
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	scanf("%d", &n);
+	if (n < 0)
+	{
+	printf("%d is negative", n);
+	}
+	if (n > 0)
+	{
+	printf("%d is positive", n);
+	}
+	if (n == 0)
+	{
+	printf("%d is zero", n);
+	}
+	return (0);
+}
