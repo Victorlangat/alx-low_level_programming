c40c049e782c87335808443c72192d61c65be3d8
Victorlangat <victorjameskibet09@gmail.com>
2023-09-14 23:26:43 -0700

    printing alphabets except some

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * description 'prints alphabet in lowercase except e and q'
+ * return:always 0
+ */
+int main(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		if (a == 101 && a == 113)
+		{
+			a++;
+			continue;
+		}
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
