cf4dcedcba1d1fe2a2362545ac26a1cebb2a26c0
Victorlangat <victorjameskibet09@gmail.com>
2023-09-20 04:42:33 -0700

    reseting

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Description: takes a pointer to an int as parameter
+ * updates the value it points
+ *
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
