66a9dc6e3ac2c5bd5846eb26aca5cc26f99dcac4
Victorlangat <victorjameskibet09@gmail.com>
2023-09-20 22:09:00 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+ * puts_half - prints half a string
+ * if odd n = (length_of_the_string - 1) / 2
+ * Return: half of input
+ */
+void puts_half(char *str)
+{
+	int a, n, longstr;
+	longstr = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		longstr++;
+
+	n = (longstr / 2)
+
+		if ((lonstr % 2) == 1)
+			n = (length_of_the_string - 1) / 2;
+
+	for (a = n; str[a] != '\0'; a++)
+	{
+		_pitchar(str[a]);
+	}
+	_putchar('\n');
+}
