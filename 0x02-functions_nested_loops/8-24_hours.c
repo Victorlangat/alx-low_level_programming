91fe1167a0e06463e6a3e78891010ae17afb62e5
Victorlangat <victorjameskibet09@gmail.com>
2023-09-21 05:38:03 -0700

    printing every minute of day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * jack_bauer - Prints every minute of the day of jack bauer
- * Return: void
+ * Return: ..
  */
 void jack_bauer(void)
 {
@@ -11,8 +11,8 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			if ((a <= 1 && b >= 9) || (a <= 2; && b <= 3))
-			{
+			if ((a <= 1 && b >= 9) || (a <= 2 && b <= 3))
+			{			{
 				for (c = 0; c <= 5; c++)
 				{
 					for (d = 0; d <= 9; d++)
