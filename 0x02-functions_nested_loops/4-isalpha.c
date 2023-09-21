d86a99dcc54e4afaee0e5e3b2ed7f4885b96d321
Victorlangat <victorjameskibet09@gmail.com>
2023-09-21 03:45:41 -0700

    identifying alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabet character
+ * @b: character
+ * Return: 1 for alphabet or else 0
+ */
+int _isalpha(int b)
+{
+	if ((b >= 65 && b <= 97) || (b >= 97 && b <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
+		
