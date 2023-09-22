a88248cd05ff60f361288237a9cafd3bc849d6bf
Victorlangat <victorjameskibet09@gmail.com>
2023-09-22 04:32:12 -0700

    printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_numbers - prints numbers from 1 to 9
+ *
+ *Return: ..
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
