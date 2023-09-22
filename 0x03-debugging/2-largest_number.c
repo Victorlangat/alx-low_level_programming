960877594b6df88da9f77f64bbb4991d580f2ba4
Victorlangat <victorjameskibet09@gmail.com>
2023-09-21 22:53:05 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,14 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a > b && a > c)
+    if (a > b && b > c)
     {
         largest = a;
     }
-    else if (b > a && b > c)
+    else if (b > a && a > c)
     {
         largest = b;
     }
-    else if (c > a)
-    {
-	    largest = c;
     else
     {
         largest = c;
